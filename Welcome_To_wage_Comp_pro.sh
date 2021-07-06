38a475795d2944c51582e6ece10a20e14435da73
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-06 21:53:32 +0530

    Added

--- Welcome_To_wage_Comp_pro.sh
+++ Welcome_To_wage_Comp_pro.sh
@@ -28,3 +28,13 @@ fi
 sal=$(($salperhr*$fullday))
 
 echo "Salary is "$sal
+
+# By case
+
+case "$rand" in 0) echo "You  were Present full day salary is 160" ;;
+   1) echo "You were Half full day salary is 80"
+   ;;
+   2) echo "Sorry You were Absent "
+   ;;
+
+esac
