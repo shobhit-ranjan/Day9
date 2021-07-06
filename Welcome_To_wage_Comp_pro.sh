4082bf423dd5a1175d77ff1d487baed03ac630d8
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-06 21:33:26 +0530

    Added

--- Welcome_To_wage_Comp_pro.sh
+++ Welcome_To_wage_Comp_pro.sh
@@ -5,10 +5,21 @@ echo " Welcome to Employee Wage Computation Program"
 
 rand=$((RANDOM%2))
 
+salperhr=20
+fullday=0
+
+
 echo $rand
 if [ $rand -eq 0 ]
    then
     echo " Present "
+      fullday=8
 else
-  echo" Absent "
+    echo "Absent"
+
 fi
+
+
+sal=$(($salperhr*$fullday))
+
+echo "Salary is "$sal
