fe171883472ffa9b11251cb6ca17d8d19982a6cd
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-07 09:57:21 +0530

    Addition

--- Welcome_To_wage_Comp_pro.sh
+++ Welcome_To_wage_Comp_pro.sh
@@ -1,43 +1,32 @@
 
 
-echo " Welcome to Employee Wage Computation Program"
 
 
-salperhr=20
-fullday=0
-workingDay=20
-working=0
-totalday=0
 
-while [ $working -lt 100 ]  && [ $totalday -lt 20 ]
-do
- for ((i=0;i<20;i++))
- do
-  rand=$((RANDOM%3))
+function getworkinghr()
+{
+# getting randome no at $1 and now cheaking the value and returning workinghr.
+   if [ $1 -eq 0 ]
+   then
+         workingHr=8
 
 
-	echo $rand
-	if [ $rand -eq 0 ]
-	   then
-	      echo " Present "
-	       fullday=8
-                totalday=$(($totalday+1))
-	elif [ $rand -eq 1 ]
-	   then
-      	     echo "Half Day you were there."
-	     fullday=4
-	else
-             echo "Absent"
-             fullday=0
+   elif [ $1 -eq 1 ]
+   then
+         workingHr=4
+   else
+         workingHr=0
 
-	fi
+   fi
+ echo $workingHr
+}
+#passing random no to function from 0 to 3
+workingHr="$( getworkinghr $((RANDOM%3)))"
 
 
-	sal=$((sal+($salperhr*$fullday)))
-        working=$((working +$fullday))
- done
-done
-echo "Total salary of 20 is "$sal
+
+
+echo "working hr of person is "$workingHr
 
 
 
@@ -45,14 +34,3 @@ echo "Total salary of 20 is "$sal
 
 
 
-
-
-# By case
-
-case "$rand" in 0) echo "You  were Present full day salary is 160" ;;
-   1) echo "You were Half full day salary is 80"
-   ;;
-   2) echo "Sorry You were Absent "
-   ;;
-
-esac
